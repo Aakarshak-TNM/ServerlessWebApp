06f1e40653a273c7113972c6f1dd4a4be3ba5a0a
Aakarshak-TNM <aakarshak.s@technmanconsulting.com>
2024-03-06 18:38:54 -0500

    Added Config.js!-2

--- dist/js/config.js
+++ dist/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-west-2_uXboG5pAb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '25ddkmj4v6hfsfvruhpfi7n4hv', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-west-2' // e.g. us-east-2
+        userPoolId: 'ap-south-1_TBbwzYN0f', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '244g7dhvitm255cbfrmh1r57h0g', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
